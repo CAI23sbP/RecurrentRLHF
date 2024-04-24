1f84bafda8f36fa59e06ae74697e1bdb9845b209
Sang baek, Park <108871750+CAI23sbP@users.noreply.github.com>
2024-04-25 01:29:40 +0900

    Update dict_preference.py
    add dict recurrent preference model to support dict obs type

--- dict_preference.py
+++ dict_preference.py
@@ -7,44 +7,65 @@ T = TypeVar("T")
 from imitation.algorithms.preference_comparisons import PreferenceModel, get_base_model
 from imitation.rewards import reward_nets 
 import torch.nn as nn 
+from imitation.data.types import Transitions, assert_not_dictobs
+from imitation.util import util
+import torch as th 
+import recurrent_preference 
+from common.data.recurrent_types import RecurrentTransitions
 
-class DictPreferenceModel(nn.Module):
+class DictPreferenceModel(PreferenceModel):
     """Class to convert two fragments' rewards into preference probability."""
 
-    def __init__(
-        self,
-        model: reward_nets.RewardNet,
-        noise_prob: float = 0.0,
-        discount_factor: float = 1.0,
-        threshold: float = 50,
-    ) -> None:
-        super().__init__()
-        self.model = model
-        self.noise_prob = noise_prob
-        self.discount_factor = discount_factor
-        self.threshold = threshold
-        base_model = get_base_model(model)
-        self.ensemble_model = None
-
-        if isinstance(base_model, reward_nets.RewardEnsemble):
-            is_base = model is base_model
-            is_std_wrapper = (
-                isinstance(model, reward_nets.AddSTDRewardWrapper)
-                and model.base is base_model
+    def rewards(self, transitions: Transitions) -> th.Tensor:
+        state = transitions.obs
+        action = transitions.acts
+        next_state = transitions.next_obs
+        done = transitions.dones
+        if self.ensemble_model is not None:
+            rews_np = self.ensemble_model.predict_processed_all(
+                state,
+                action,
+                next_state,
+                done,
             )
+            assert rews_np.shape == (len(state), self.ensemble_model.num_members)
+            rews = util.safe_to_tensor(rews_np).to(self.ensemble_model.device)
 
-            if not (is_base or is_std_wrapper):
-                raise ValueError(
-                    "RewardEnsemble can only be wrapped"
-                    f" by AddSTDRewardWrapper but found {type(model).__name__}.",
-                )
-            self.ensemble_model = base_model
-            self.member_pref_models = []
-            for member in self.ensemble_model.members:
-                member_pref_model = PreferenceModel(
-                    cast(reward_nets.RewardNet, member),  # nn.ModuleList is not generic
-                    self.noise_prob,
-                    self.discount_factor,
-                    self.threshold,
-                )
-                self.member_pref_models.append(member_pref_model)
+        else:
+            preprocessed = self.model.preprocess(state, action, next_state, done)
+            rews = self.model(*preprocessed)
+            for key, state_ in state.items():
+                if isinstance(state, dict):
+                    assert rews.shape == (len(state_[key]),)
+        return rews
+
+class DictRecurrentPreferenceModel(recurrent_preference.RecurrentPreferenceModel):
+    
+    def rewards(self, transitions: RecurrentTransitions) -> th.Tensor:
+
+        state = assert_not_dictobs(transitions.obs)
+        action = transitions.acts
+        next_state = assert_not_dictobs(transitions.next_obs)
+        done = transitions.dones
+        hidden_state = transitions.hidden_states
+        if self.ensemble_model is not None:
+            rews_np, _ = self.ensemble_model.predict_processed_all(
+                state,
+                action,
+                next_state,
+                done,
+                hidden_state
+            )
+            assert rews_np.shape == (len(state), self.ensemble_model.num_members)
+            rews = util.safe_to_tensor(rews_np).to(self.ensemble_model.device)
+
+        else:
+            if len(hidden_state.shape)>3:
+                hidden_state = hidden_state[self.member_indx].swapaxes(0,1)
+            preprocessed = self.model.preprocess(state, action, next_state, done, hidden_state)
+            rews, _ = self.model(*preprocessed)
+            for key, state_ in state.items():
+                if isinstance(state, dict):
+                    assert rews.shape == (len(state_[key]),)
+        return rews
+    

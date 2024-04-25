b14bec502bbc2ee716a87dbb02112cfc7176f1e2
Sang baek, Park <108871750+CAI23sbP@users.noreply.github.com>
2024-04-25 15:18:24 +0900

    Update dict_preference.py

--- dict_preference.py
+++ dict_preference.py
@@ -4,9 +4,7 @@ from typing import (
 
 )
 T = TypeVar("T")
-from imitation.algorithms.preference_comparisons import PreferenceModel, get_base_model
-from imitation.rewards import reward_nets 
-import torch.nn as nn 
+from imitation.algorithms.preference_comparisons import PreferenceModel
 from imitation.data.types import Transitions, assert_not_dictobs
 from imitation.util import util
 import torch as th 
@@ -14,7 +12,6 @@ import recurrent_preference
 from common.data.recurrent_types import RecurrentTransitions
 
 class DictPreferenceModel(PreferenceModel):
-    """Class to convert two fragments' rewards into preference probability."""
 
     def rewards(self, transitions: Transitions) -> th.Tensor:
         state = transitions.obs
@@ -39,33 +36,3 @@ class DictPreferenceModel(PreferenceModel):
                     assert rews.shape == (len(state_[key]),)
         return rews
 
-class DictRecurrentPreferenceModel(recurrent_preference.RecurrentPreferenceModel):
-    
-    def rewards(self, transitions: RecurrentTransitions) -> th.Tensor:
-
-        state = transitions.obs
-        action = transitions.acts
-        next_state = transitions.next_obs
-        done = transitions.dones
-        hidden_state = transitions.hidden_states
-        if self.ensemble_model is not None:
-            rews_np, _ = self.ensemble_model.predict_processed_all(
-                state,
-                action,
-                next_state,
-                done,
-                hidden_state
-            )
-            assert rews_np.shape == (len(state), self.ensemble_model.num_members)
-            rews = util.safe_to_tensor(rews_np).to(self.ensemble_model.device)
-
-        else:
-            if len(hidden_state.shape)>3:
-                hidden_state = hidden_state[self.member_indx].swapaxes(0,1)
-            preprocessed = self.model.preprocess(state, action, next_state, done, hidden_state)
-            rews, _ = self.model(*preprocessed)
-            for key, state_ in state.items():
-                if isinstance(state, dict):
-                    assert rews.shape == (len(state_[key]),)
-        return rews
-    
